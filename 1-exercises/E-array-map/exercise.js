e29ccd677bcf06808d6569e862fa5eef62e290c0
Domenico <domenico.cyf@gmail.com>
2021-02-11 22:13:03 +0000

    Exercise done until E

--- 1-exercises/E-array-map/exercise.js
+++ 1-exercises/E-array-map/exercise.js
@@ -2,3 +2,24 @@
 // Write multiple solutions using different syntax (as shown in the README)
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+
+
+
+
+var mulByHundredCaseOne = numbers.map(function mul(num) {
+    return num * 100;
+    })
+    console.log(mulByHundredCaseOne);
+
+var mulByHundredCaseTwo =  numbers.map(function (num) {
+    return num * 100;
+    })
+console.log(mulByHundredCaseTwo);
+
+var mulByHundredCaseThree = numbers.map(number => {
+    return number *100});
+console.log(mulByHundredCaseThree);
+
+
+var mulByHundredCaseFour = numbers.map(number => number *100);
+console.log(mulByHundredCaseFour);
